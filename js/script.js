cfe086d6d27d005f8efe9a3444723cd985e2fb45
David Bolu <66206100+GIT-Gizmo@users.noreply.github.com>
2023-05-01 01:14:10 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function removeNav() {
+
+    // document.getElementById('nav').style.position = 'relative';
+    // document.getElementById('nav').style.right = '100vw';
+
+    document.getElementById('check').classList.remove('checkbox');
+    // document.getElementsByClassName('menu').classList.add('navbar-nav');
+
+}
+
+function addNav() {
+
+    // document.getElementById('nav').style.position = 'relative';
+    // document.getElementById('nav').style.right = '100vw';
+
+    document.getElementById('check').classList.add('checkbox');
+    // document.getElementsByClassName('menu').classList.add('navbar-nav');
+
+}
